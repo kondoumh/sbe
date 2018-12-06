966c57d18b0cb6c024e5a082b8ab6a75df33e8a4
kondoumh <kondoumh@gmail.com>
2018-12-07 06:38:34 +0900

    WIP apply electron-tab

--- renderer.js
+++ renderer.js
@@ -1,8 +1,7 @@
 const electron = require("electron");
+const TabGroup = require("electron-tabs");
 
 let webview
-
-//const TabGroup = require("electron-tabs");
 
 const goBack = () => {
   if (webview && webview.canGoBack()) {
@@ -16,49 +15,51 @@ const goForward = () => {
   }
 }
 
+let tabGroup = new TabGroup();
+let tab = tabGroup.addTab({
+    title: "Electron",
+    src: "https://scrapbox.io/",
+    visible: true,
+    active: true
+});
+
 onload = () => {
-  webview = document.getElementById("webview");
+  
+  //webview = document.getElementById("webview");
 
-  webview.addEventListener("new-window", e => {
-    electron.shell.openExternal(e.url);
-  });
-  document.querySelector("#btn_back").addEventListener('click', (event) => {
-    goBack();
-  });
-  document.querySelector("#btn_forward").addEventListener('click', (event) => {
-    goForward();
-  });
-  document.querySelector("#btn_reload").addEventListener('click', (event) => {
-    webview.reload();
-  });
-
-  // let tabGroup = new TabGroup();
-  // let tab = tabGroup.addTab({
-  //     title: "Electron",
-  //     src: "http://electron.atom.io",
-  //     visible: true
+  // webview.addEventListener("new-window", e => {
+  //   electron.shell.openExternal(e.url);
+  // });
+  // document.querySelector("#btn_back").addEventListener('click', (event) => {
+  //   goBack();
+  // });
+  // document.querySelector("#btn_forward").addEventListener('click', (event) => {
+  //   goForward();
+  // });
+  // document.querySelector("#btn_reload").addEventListener('click', (event) => {
+  //   webview.reload();
   // });
 };
 
-const { ipcRenderer } = require("electron");
+// const { ipcRenderer } = require("electron");
 
-const ElectronSearchText = require("electron-search-text");
-const searcher = new ElectronSearchText({
-  target: "#webview",
-  input: ".search-input",
-  count: ".search-count",
-  box: ".search-box",
-  visibleClass: ".state-visible"
-});
+// const ElectronSearchText = require("electron-search-text");
+// const searcher = new ElectronSearchText({
+//   target: "#webview",
+//   input: ".search-input",
+//   count: ".search-count",
+//   box: ".search-box",
+//   visibleClass: ".state-visible"
+// });
 
-ipcRenderer.on("toggleSearch", () => {
-  searcher.emit("toggle");
-});
+// ipcRenderer.on("toggleSearch", () => {
+//   searcher.emit("toggle");
+// });
 
-ipcRenderer.on("goBack", () => {
-  goBack();
-});
+// ipcRenderer.on("goBack", () => {
+//   goBack();
+// });
 
-ipcRenderer.on("goForward", () => {
-  goForward();
-});
+// ipcRenderer.on("goForward", () => {
+//   goForward();
+// });
