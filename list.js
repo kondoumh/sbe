305db4cd414db73e84ed68455d15582804001d97
kondoumh <kondoumh@gmail.com>
2019-01-20 23:39:11 +0900

    WIP fetch page titles

--- list.js
+++ list.js
@@ -1,23 +1,23 @@
 const BASE_URL = "https://scrapbox.io/";
 
-function getPageTitles() {
+function getPageTitles(direction) {
   const titles = document.getElementById("titles");
   const status = document.getElementById("sbe_paging");
   const projectName = localStorage.getItem("projectName");
-  const pagesUrl = BASE_URL + "api/pages/" + projectName;
+  const skip = sessionStorage.getItem("skip");
+  const start = skip ? parseInt(skip) : 1;
+  const pagesUrl = BASE_URL + "api/pages/" + projectName + "?skip=" + (start - 1) + "&limit=100";
   fetch(pagesUrl, {
     credentials: "include"
   })
     .then(res => {
       if (res.status === 200) {
-        const skip = sessionStorage.getItem("skip");
-        const start = skip ? parseInt(skip) : 1;
         res.json().then(data => {
           const limit = parseInt(data.limit);
           const end = start + limit - 1;
           const total = parseInt(data.count);
-          status.innerHTML =
-            start + " - " + end + " total:" + total + "<br>";
+          status.innerHTML = start + " - " + end + " total:" + total + "<br>";
+          titles.innerHTML = direction + "<br>";
           Object.keys(data.pages).forEach(key => {
             titles.innerHTML +=
               "<a href=" +
@@ -27,7 +27,11 @@ function getPageTitles() {
               encodeURI(data.pages[key].title) +
               ">" +
               data.pages[key].title +
-              "</a> / " + data.pages[key].views + " views  / " + data.pages[key].linked + " linked<br>";
+              "</a> / " +
+              data.pages[key].views +
+              " views  / " +
+              data.pages[key].linked +
+              " linked<br>";
           });
           if (start + limit < total) {
             sessionStorage.setItem("skip", start + limit);
