098cc04ce6174a5baba749d4841a6590c1c45463
kondoumh <kondoumh@gmail.com>
2020-01-29 15:25:48 +0900

    WIP convert codeblock and table

--- src/Markdown.js
+++ src/Markdown.js
@@ -1,3 +1,6 @@
+let codeblock = false;
+let table = false;
+
 function toMarkdown(lines) {
   let content = "";
   lines.forEach(line => {
@@ -7,19 +10,53 @@ function toMarkdown(lines) {
 }
 
 function convert(line) {
+  const rgxCodeBlock = /^code:([^.]*)(\.([^.]*))?/
+  const rgxTable = /^table:(.*)$/
   const rgxHeading = /^\[(\*+)\s([^\]]+)\]/;
   const rgxIndent = /^(\s+)([^\s].+)/;
   const rgxStrong = /\[(\*+)\s(.+)\]/g;
   let result = "";
-  if (rgxHeading.test(line)) {
-    const ar = rgxHeading.exec(line);
-    result = "#".repeat(decideLevel(ar[1].length)) + " " + ar[2];
-  } else if (rgxIndent.test(line)) {
-    const ar = rgxIndent.exec(line);
-    const indent = "  ".repeat(ar[1].length - 1);
-    result = indent + "- " + ar[2].replace(rgxStrong, "**$2**");
-  } else {
-    result = line.replace(rgxStrong, "**$2**");
+  if (codeblock) {
+    if (!line.startsWith(" ")) {
+      result = "```\n"
+      codeblock = false;
+    } else {
+      result = line;
+      return result;
+    }
+  } else if (table) {
+    if (!line.startsWith(" ")) {
+      result = "end table\n";
+      table = false;
+    } else {
+      result = line;
+      return result;
+    }
+  }
+  if (!codeblock && !table) {
+    if (rgxCodeBlock.test(line)) {
+      const ar = rgxCodeBlock.exec(line);
+      console.log(ar);
+      codeblock = true;
+      result = ar[1] + (ar[2] ? ar[2] : "") + "\n```" + (ar[3] ? ar[3] : "");
+      return result;
+    }
+    if (rgxTable.test(line)) {
+      const ar = rgxTable.exec(line);
+      table = true;
+      result = "start table";
+      return result;
+    }
+    if (rgxHeading.test(line)) {
+      const ar = rgxHeading.exec(line);
+      result += "#".repeat(decideLevel(ar[1].length)) + " " + ar[2];
+    } else if (rgxIndent.test(line)) {
+      const ar = rgxIndent.exec(line);
+      const indent = "  ".repeat(ar[1].length - 1);
+      result += indent + "- " + ar[2].replace(rgxStrong, "**$2**");
+    } else {
+      result += line.replace(rgxStrong, "**$2**");
+    }
   }
   return result;
 }
